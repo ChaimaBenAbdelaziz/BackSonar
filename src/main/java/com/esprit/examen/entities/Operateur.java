07b05b488c075957ee6b08a185476b233e204b6e
ChaimaBa <chaima.benabdelaziz@esprit.com.tn>
2022-11-14 15:05:38 +0100

    up

--- src/main/java/com/esprit/examen/entities/Operateur.java
+++ src/main/java/com/esprit/examen/entities/Operateur.java
@@ -6,7 +6,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Operateur implements Serializable{
 	private static final long serialVersionUID = 1L;
 
@@ -15,57 +24,5 @@ public class Operateur implements Serializable{
 	private Long idOperateur;
 	private String nom;
 	private String prenom;
-	
 	private String password;
-
-
-	public Operateur() {
-		super();
-	}
-
-	public Operateur(Long idOperateur, String nom, String prenom, String password) {
-		super();
-		this.idOperateur = idOperateur;
-		this.nom = nom;
-		this.prenom = prenom;
-		this.password = password;
-	}
-
-	public Long getIdOperateur() {
-		return idOperateur;
-	}
-
-	public void setIdOperateur(Long idOperateur) {
-		this.idOperateur = idOperateur;
-	}
-
-	public String getNom() {
-		return nom;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	public String getPrenom() {
-		return prenom;
-	}
-
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-	
-	
 }
--- src/main/java/com/esprit/examen/entities/OperateurRequest.java
+++ src/main/java/com/esprit/examen/entities/OperateurRequest.java
@@ -1,44 +1,18 @@
 package com.esprit.examen.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class OperateurRequest {
 
 	private Long idOperateur;
 	private String nom;
 	private String prenom;
-	
 	private String password;
-
-	public Long getIdOperateur() {
-		return idOperateur;
-	}
-
-	public void setIdOperateur(Long idOperateur) {
-		this.idOperateur = idOperateur;
-	}
-
-	public String getNom() {
-		return nom;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	public String getPrenom() {
-		return prenom;
-	}
-
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
 }
